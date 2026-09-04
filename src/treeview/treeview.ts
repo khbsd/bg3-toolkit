a5ce9b480570cc7eddafc1d47dc7031e823f6e1c
khbsd <kayabeans@proton.me>
2026-09-04 15:05:21 -0500

    feat: dynamic command loading

--- src/treeview/treeview.ts
+++ src/treeview/treeview.ts
@@ -4,16 +4,11 @@ import * as path from 'path';
 export class ToolkitTreeProvider implements vscode.TreeDataProvider<ToolkitTreeView> {
   constructor(private workspaceRoot: string | undefined) {}
   getTreeItem(element: ToolkitTreeView): vscode.TreeItem {
-    console.log(this.workspaceRoot);
-    console.log(element);
     return element;
   }
   getChildren(element?: ToolkitTreeView): Thenable<ToolkitTreeView[]> {
-    console.log(this.workspaceRoot);
-    console.log(element);
-
     return Promise.resolve([]);
- }
+  }
 }
 
 class ToolkitTreeView extends vscode.TreeItem {
@@ -21,7 +16,7 @@ class ToolkitTreeView extends vscode.TreeItem {
       super(path);
   }
 
-  tkIconPath: vscode.Uri = vscode.Uri.parse(__filename.concat('..', '..', 'resources', 'icon', 'icon.svg'));
+  tkIconPath: vscode.Uri = vscode.Uri.parse(__filename.concat(path.join('..', '..', 'resources', 'icon', 'icon.svg')));
   // icon attribution:
   // - square brackets ("[", "]"): https://github.com/tonsky/FiraCode, Fira Code OFL license
   // - d20 vector: https://opensvg.dev/icons/action?prefix=fa-solid&icon=dice-d20, by dave gandy (c) CC BY 4.0
@@ -30,8 +25,4 @@ class ToolkitTreeView extends vscode.TreeItem {
     light: this.tkIconPath,
     dark: this.tkIconPath
   };
-
-  iconCheck(): void {
-    console.log(this.tkIconPath);
-  }
 }
