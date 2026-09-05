9378aeb73b234d96d517b70450676e6844d8f8ae
khbsd <kayabeans@proton.me>
2026-09-04 19:16:38 -0500

    feat: a couple intro views, wip workspace pathing

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
-import { Registrar } from './command/register';
+import * as path from 'path';
+import { Registrar } from './command/registrar';
 import { ToolkitTreeProvider } from './treeview/treeview';
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -13,16 +14,23 @@ export async function activate(context: vscode.ExtensionContext) {
 	}
 
 	// set up treeview
-	let wsPath = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
-    ? vscode.workspace.workspaceFolders[0].uri.fsPath
-    : undefined;
-	vscode.window.registerTreeDataProvider(
-  	'toolkitTree',
-  	new ToolkitTreeProvider(wsPath)
-	);
-	vscode.window.createTreeView('toolkitTree', {
+	let wsPath: string;
+
+	if (vscode.workspace.workspaceFolders?.length !== undefined && vscode.workspace.workspaceFolders.length > 0) {
+		wsPath = vscode.workspace.workspaceFolders[0].uri.toString();
+	} else {
+		wsPath = "home";
+	}
+	let tvWelcome = vscode.window.createTreeView('toolkitTree', {
   		treeDataProvider: new ToolkitTreeProvider(wsPath),
 	});
+	let tvWorkspace = vscode.window.createTreeView('toolkitTreeWorkspace', {
+  		treeDataProvider: new ToolkitTreeProvider(wsPath),
+	});
+
+	let wsPathShort = wsPath.split(path.sep).slice(-4);
+	console.log(wsPathShort);
+	tvWorkspace.message = "\nyour workspace is:\n\n" + wsPathShort.forEach((p) => {console.log(p);return path.join(p, path.sep);});
 	
 }
 
--- src/treeview/treeview.ts
+++ src/treeview/treeview.ts
@@ -2,18 +2,28 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export class ToolkitTreeProvider implements vscode.TreeDataProvider<ToolkitTreeView> {
-  constructor(private workspaceRoot: string | undefined) {}
+  constructor(private workspaceRoot: string) {}
   getTreeItem(element: ToolkitTreeView): vscode.TreeItem {
     return element;
   }
   getChildren(element?: ToolkitTreeView): Thenable<ToolkitTreeView[]> {
-    return Promise.resolve([]);
+    if (element) {
+      element.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+      return Promise.resolve([element]);
+    }
+    else {
+      return Promise.resolve([]);
+    }
   }
 }
 
 class ToolkitTreeView extends vscode.TreeItem {
-  constructor (public readonly path: string) {
-      super(path);
+  constructor (public readonly path: string, public collapse: vscode.TreeItemCollapsibleState) {
+    path = path ?? __dirname;
+    super(path, collapse);
+    this.label = path;
+    this.description = path;
+    this.collapsibleState = collapse;
   }
 
   tkIconPath: vscode.Uri = vscode.Uri.parse(__filename.concat(path.join('..', '..', 'resources', 'icon', 'icon.svg')));
