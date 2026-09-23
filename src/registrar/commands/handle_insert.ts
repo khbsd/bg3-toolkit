b0837018702e5a8418c20c9c921a595a3f944828
khbsd <kayabeans@proton.me>
2026-09-22 21:22:05 -0500

    patch(0.4.1): fix uuid/handle insertion

--- src/registrar/commands/handle_insert.ts
+++ src/registrar/commands/handle_insert.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { Handle } from "../../utils/uuid_handle";
-import { getSelectionOrCursorWord, replaceInFiles } from "../../utils/ws";
+import { insertAt } from "../../utils/ws";
 import { CommandCommon } from "../command_common";
 
 export class Command {
@@ -8,12 +8,11 @@ export class Command {
     const disposable = vscode.commands.registerCommand(
       "bg3-toolkit.handleInsert",
       () => {
-        const oldHandle: string = getSelectionOrCursorWord();
         const e = vscode.window.activeTextEditor;
         if (e === undefined) {
           return;
         }
-        replaceInFiles([e.document.uri], oldHandle, new Handle().handle);
+        insertAt(new Handle().handle, e.selection);
       },
     );
     return disposable;
--- src/registrar/commands/uuid_insert.ts
+++ src/registrar/commands/uuid_insert.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { Uuid } from "../../utils/uuid_handle";
-import { getSelectionOrCursorWord, replaceInFiles } from "../../utils/ws";
+import { insertAt } from "../../utils/ws";
 import { CommandCommon } from "../command_common";
 
 export class Command {
@@ -8,12 +8,11 @@ export class Command {
     const disposable = vscode.commands.registerCommand(
       "bg3-toolkit.uuidInsert",
       () => {
-        const oldUuid: string = getSelectionOrCursorWord();
         const e = vscode.window.activeTextEditor;
         if (e === undefined) {
           return;
         }
-        replaceInFiles([e.document.uri], oldUuid, new Uuid().uuid);
+        insertAt(new Uuid().uuid, e.selection);
       },
     );
     return disposable;
